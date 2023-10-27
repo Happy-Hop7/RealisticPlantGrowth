02c40482732e374ad629473402df9d110c12076c
PredatorG7 <lord.kampfkeks@gmail.com>
2023-10-28 00:37:32 +0200

    Setting up project structure

---
+++ src/main/java/de/nightevolution/MessageManager.java
@@ -0,0 +1,10 @@
+package de.nightevolution;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+
+public class MessageManager {
+
+    //todo: support locals and minimassage format
+}
