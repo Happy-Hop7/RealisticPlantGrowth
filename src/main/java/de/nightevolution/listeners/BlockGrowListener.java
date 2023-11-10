51c6851e1b827a2f63438a227843fca845ec639e
TheRealPredator <lord.kampfkeks@gmail.com>
2023-11-10 01:18:40 +0100

    Restructured GrowthModificators.yml
    Need to reimplement all modificators with updated Materials and Biomes

--- src/main/java/de/nightevolution/listeners/BlockGrowListener.java
+++ src/main/java/de/nightevolution/listeners/BlockGrowListener.java
@@ -1,9 +1,20 @@
 package de.nightevolution.listeners;
 
 import de.nightevolution.RealisticPlantGrowth;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockGrowEvent;
 
 /**
  * Listens to calls when a block grows naturally in the world.
+ *  Examples:
+ *     Wheat,
+ *     Sugar Cane,
+ *     Cactus,
+ *     Watermelon,
+ *     Pumpkin,
  * Used to manipulate growth rates of plants.
  */
 public class BlockGrowListener extends PlantGrowthListener{
@@ -13,6 +24,14 @@ public class BlockGrowListener extends PlantGrowthListener{
         logger.verbose("Registered new " + this.getClass().getSimpleName() + ".");
     }
 
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = false)
+    public void onPlantGrow(BlockGrowEvent e){
+        Block eventBlock = e.getBlock();
+        World world = eventBlock.getWorld();
 
+        if(configManager.getEnabled_worlds().contains(world.getName()))
+            return;
+
+    }
 
 }
